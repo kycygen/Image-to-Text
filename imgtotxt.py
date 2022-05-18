d9ef622f97e788da1988d02a312dbf7a6829e601
kycygen <102585967+kycygen@users.noreply.github.com>
2022-05-18 17:04:31 +0800

    Update imgtotxt.py

--- imgtotxt.py
+++ imgtotxt.py
@@ -1,8 +1,9 @@
 #Install py tesseract first by typing "pip install pytesseract"
+#If you are getting an error regarding pytesseract, download pytesseract manually on this link "https://github.com/UB-Mannheim/tesseract/wiki"
 
 import pytesseract as tess
 #Insert the file location of "tesseract.exe"
-tess.pytesseract.tesseract_cmd = r''
+tess.pytesseract.tesseract_cmd = r'C:\Users\Your_User\AppData\Local\Tesseract-OCR\tesseract.exe'
 from PIL import Image
 
 #Insert the file name of your image
